d426424aeb659bfd584d3b63c8fd0470b41996cd
mebranemlm <none>
2016-04-26 16:55:13 -0500

    $mensaje

--- assets/js/fundamentos.js
+++ assets/js/fundamentos.js
@@ -6,21 +6,44 @@ var $nombreEspecial =  23;
 var a348944323 = 'Variable Con Números';
 
 //Variable Undefined
-var variable;
-console.log(variable === undefined);
+$('#varUndefined').click(function(){
+	var variable;
+	console.log(variable === undefined);
+});
+
 
 //IndexOf y CharAt
-var texto1='hola mundo';
-var texto2='hola Mundo';
-var texto3='hola MUNDO MUNDO';
+$('#indexOfCharAt').click(function(){
+	var texto1='hola mundo';
+	var texto2='hola Mundo';
+	var texto3='hola MUNDO MUNDO';
 
-var busqueda='MUNDO';
-console.log(texto3.indexOf(busqueda));
+	var busqueda='MUNDO';
+	console.log(texto3.indexOf(busqueda));
 
-console.log(texto3.charAt(5));
+	console.log(texto3.charAt(5));
+});
 
 //Redondeos
 Math.round(1.1);
 Math.floor(1.1);
 Math.ceil(1.1);
 
+//Arrays
+
+$('#arrays').click(function(){
+	arrLista = [1,'Pedro',42,function(a,b){return a+b}];
+	console.log(arrLista);
+	console.log(arrLista.toString());
+
+	arrLista.push('Último');
+	arrLista.unshift('Primero');
+
+	console.log(arrLista);
+
+	arrLista.pop(); //Delete Last
+	console.log(arrLista);
+
+	arrLista.shift(); //Delete First
+	console.log(arrLista);
+});
